7d8c060063ea3f44c575262e50558623841e30b3
ramuering <117507052+ramuering@users.noreply.github.com>
2023-07-23 21:58:11 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,17 @@
+from math import gcd
+def factor(x):
+    chk=2
+    while chk<=x:
+        if x%chk==0:
+            x=x//chk
+            if chk!=2 and chk!=5:
+                return 2
+        else:
+            chk+=1
+    return 1
+def solution(a, b):
+    n=gcd(a,b)
+    a=a//n
+    b=b//n
+    
+    return factor(b)
