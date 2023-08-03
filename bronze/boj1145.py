0ff0ff4c58131861c157c76dce4770aeced88013
ramuering <sugarone0426@gmal.com>
2023-08-03 15:24:26 +0900

    적어도 대부분의 배수

--- bronze/boj1145.py
+++ bronze/boj1145.py
@@ -1,17 +1,14 @@
-from itertools import combinations
-import math
 nums=list(map(int,input().split()))
-ans=[]
-if 1 in nums:
-  nums = [i for i in nums if i>1]
-  for a,b in combinations(nums,3-(5-len(nums))):
-    tmp=math.gcd(a,b)
-    ans.append(a*b//tmp)
-  print(min(ans))
-else: 
-  for a,b,c in list(combinations(nums,3)):
-    tmp1=math.gcd(a,b)
-    tmp2=math.gcd(tmp1,c)
-    tmp3=a*b//tmp1
-    ans.append(tmp3*c//tmp2)
-  print(min(ans))  
+cnt=0
+i=1
+while True:
+  for num in nums:
+    if i%num==0:
+      cnt+=1
+  if cnt>=3:
+    break
+  cnt=0
+  i+=1
+print(i)
+  
+  
